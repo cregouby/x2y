0aafd3d44252d2a4686d05b55ac6f3494b52e9d7
Christophe Regouby <christophe.regouby@free.fr>
2021-09-27 13:39:18 +0200

    add support for high level of missing initial support for list vectors

--- R/x2y.R
+++ R/x2y.R
@@ -8,15 +8,16 @@
 #' @param keep method to choose the column to keeep. only "first" is implemented
 #'
 #' @return a vector of column names from the original dataframe
+#' @importFrom rlang abort
 #' @export
 #'
 #' @examples
 #'
 get_high_mi_cols <- function(dx2y_df, threshold=99, keep="first") {
   if (keep!="first") {
-    rlang::abort("'longest' method is not implemented yet")
+    abort("'longest' method is not implemented yet")
   }
-  dx2y_df %>% filter(x2y >= threshold) %>% dplyr::pull(y) %>% unique
+  dx2y_df %>% filter(x2y >= threshold) %>% pull(y) %>% unique
 }
 
 calc_mae_reduction <- function(y_hat, y_actual) {
@@ -57,12 +58,16 @@ x2y_inner <- function(x, y) {
 
   # if y is continuous
   if (is.numeric(y) && !is.factor(y)) {
-    preds <- predict(rpart::rpart(y ~ x, method = "anova"), type = 'vector')
+    preds <- predict(rpart(y ~ x, method = "anova"), type = 'vector')
     calc_mae_reduction(preds, y)
   }
+  # naive cast of list vectors
+  if (mode(x)=="list") x <- as.character(x)
+  if (mode(y)=="list") y <- as.character(y)
+
   # if y is categorical
   else {
-    preds <- predict(rpart::rpart(y ~ x, method = "class"), type = 'class')
+    preds <- predict(rpart(y ~ x, method = "class"), type = 'class')
     calc_misclass_reduction(preds, y)
   }
 }
@@ -73,22 +78,50 @@ simple_boot <- function(x,y) {
   x2y_inner(x[ids], y[ids])
 }
 
-#' Title
+#' Compute mutual information between two vectors
 #'
-#' @param x
-#' @param y
-#' @param confidence
-#' @param sample_n
+#' @param x first vector
+#' @param y second vector
+#' @param confidence boolean, shall we compute 95% confidence interval (default FALSE)
+#' @param sample_n max number of sample to consider (default 5000 )
 #'
-#' @return
+#' @return a list with `perc_of_obs` percentage of observations present in the two vectors,
+#'   `x2y` mutual information value and eventually `CI_95_Lower` and `CI_95_Upper` values.
+#' @importFrom rpart rpart
 #' @export
 #'
 #' @examples
+#' x2y(iris$Sepal.Length, iris$Species)
+#' #>$perc_of_obs
+#' #>[1] 100
+#' #>
+#' #>$x2y
+#' #>[1] 62
+#'
+#' x2y(iris$Sepal.Length, iris$Species, confidence = TRUE)
+#' #>$perc_of_obs
+#' #>[1] 100
+#' #>
+#' #>$x2y
+#' #>[1] 62
+#' #>
+#' #>$CI_95_Lower
+#' #>97.5%
+#' #>51.08
+#' #>
+#' #>$CI_95_Upper
+#' #>2.5%
+#' #>70.15
+#'
 x2y <- function(x, y, confidence = FALSE, sample_n = 5000) {
   results <- list()
 
   missing <-  is.na(x) | is.na(y)
   results$perc_of_obs <- round(100 * (1 - sum(missing) / length(x)), 2)
+  if (results$perc_of_obs==0) {
+    results$x2y <- 0
+    return(results)
+  }
 
   x <- x[!missing]
   y <- y[!missing]
@@ -122,30 +155,53 @@ x2y <- function(x, y, confidence = FALSE, sample_n = 5000) {
 
 #' Compute the mutual information between dataframe columns
 #'
-#' @param d dataframe to compare columns
+#' @param df dataframe to compare columns
 #' @param target (optional) the outcome column if any
 #' @param confidence Boolean, shall we compute the 95% Confidence interval (default FALSE)
-#' @param sample_n max number of sample to consider (default 50k )
+#' @param sample_n max number of sample to consider (default 5000 )
 #'
-#' @return a dataframe with columns `x` and `y` with all combinations of column pairs
-#'  and for each pair a `perc_of_obs` for the percentage of observations present,
-#'  and  `x2y` value of the mutual information between those two columns
+#' @return a dataframe with columns `x` and `y` filled with all combinations of column pairs
+#'  (when `target` is NA), or with all combinations of column with target (if `target` is given)
+#'  with a `perc_of_obs` column for the percentage of observations present,
+#'  and  `x2y` value of the mutual information between the two columns.
 #'
 #' @importFrom magrittr %>%
-#' @importFrom dplyr filter, pull, arrange, bind_cols, desc
+#' @importFrom dplyr filter pull arrange bind_cols desc
+#' @importFrom purrr map_dfr
+#' @importFrom future plan multisession
+#' @importFrom furrr future_map_dfr furrr_options
 #' @export
 #'
 #' @examples
 #' pairwise_iris <- dx2y(iris)
 #' head(pairwise_iris)
-dx2y <- function(d, target = NA, confidence = FALSE, sample_n=5000) {
+#' #>              x            y perc_of_obs   x2y
+#' #>1  Petal.Length      Species         100 89.94
+#' #>2   Petal.Width      Species         100 88.97
+#' #>3   Petal.Width Petal.Length         100 80.73
+#' #>4       Species Petal.Length         100 79.72
+#' #>5  Petal.Length  Petal.Width         100 77.32
+#'
+#' iris_species <- dx2y(iris, target="Species")
+#' iris_species
+#' #>x            y perc_of_obs   x2y
+#' #>1 Petal.Length      Species         100 89.94
+#' #>2  Petal.Width      Species         100 88.97
+#' #>3      Species Petal.Length         100 79.72
+#' #>4      Species  Petal.Width         100 76.31
+#' #>5 Sepal.Length      Species         100 47.09
+#' #>6      Species Sepal.Length         100 42.08
+#' #>7      Species  Sepal.Width         100 22.37
+#' #>8  Sepal.Width      Species         100 15.87
+
+dx2y <- function(df, target = NA, confidence = FALSE, sample_n=5000) {
   if (is.na(target)) {
-    pairs <- combn(ncol(d), 2)
+    pairs <- combn(ncol(df), 2)
     pairs <- cbind(pairs, pairs[2:1, ])
   }
   else {
-    n <- 1:ncol(d)
-    idx <- which(target == names(d))
+    n <- 1:ncol(df)
+    idx <- which(target == names(df))
     n <- n[n != idx]
     pairs <- cbind(rbind(n, idx), rbind(idx, n))
   }
@@ -153,31 +209,63 @@ dx2y <- function(d, target = NA, confidence = FALSE, sample_n=5000) {
   n <- dim(pairs)[2]
   p <- progressr::progressor(steps = n)
 
-  if (is.null(sample_n)) sample_n <- length(x)
-  if (length(d)>sample_n) {
-    d <- dplyr::sample_n(seq_along(x), sample_n)
+  if (is.null(sample_n)) sample_n <- nrow(df)
+  if (nrow(df)>sample_n) {
+    df <- dplyr::sample_n(seq_along(x), sample_n)
   }
 
 
-  results12 <- data.frame(x = names(d)[pairs[1,]],
-                          y = names(d)[pairs[2,]])
-  #  turn multi-process with furrr + progressr
-  furrr::furrr_options(seed=42)
-  # TODO manage system specific cases
-  future::plan(future::multisession, workers = 8)
-  results36 <- furrr::future_map_dfr(seq(n), ~{
-    p()
-    x2y(d %>% pull(pairs[1, .x]), d %>% pull(pairs[2, .x]), confidence = confidence, sample_n = sample_n)
+  results12 <- data.frame(x = names(df)[pairs[1,]],
+                          y = names(df)[pairs[2,]])
+  if (n>30) {
+    #  turn into multi-process with furrr + progressr
+    furrr_options(seed=42)
+    # TODO manage system specific cases
+    plan(multisession, workers = 8)
+    progressr::with_progress({
+      results36 <- future_map_dfr(seq(n), ~ {
+        p()
+        x2y(
+          df %>% pull(pairs[1, .x]),
+          df %>% pull(pairs[2, .x]),
+          confidence = confidence,
+          sample_n = sample_n
+        )
+      })
     })
 
+  } else {
+    progressr::with_progress({
+      results36 <- purrr::map_dfr(seq(n), ~ {
+        p()
+        x2y(
+          df %>% pull(pairs[1, .x]),
+          df %>% pull(pairs[2, .x]),
+          confidence = confidence,
+          sample_n = sample_n
+        )
+      })
+    })
+
+  }
   results <- bind_cols(results12, results36) %>%
     arrange(desc(x2y), desc(perc_of_obs))
 
   results
 }
-step_mutual_information <- function(df, mutual_information, ...) {
+#' reciepe step for removing low mutual information columns form a dataframe
+#'
+#' @param df dataframe
+#' @param mutual_information mutual information
+#' @param ... other parameters passed onto dx2y function
+#'
+#' @return
+#' @export
+#'
+#' @examples
+step_mutual_information <- function(df, mutual_information=80, ...) {
   df_x2y <- dx2y(df ,...)
-  high_mi_df <- df_x2y %>% filter(x2y >= 99)
+  high_mi_df <- df_x2y %>% filter(x2y >= mutual_information)
   high_mi_cols <- high_mi_df %>% pull(y) %>% unique
   df <- df %>% dplyr::select(-high_mi_cols)
 }
--- tests/testthat/test_x2y.R
+++ tests/testthat/test_x2y.R
@@ -1,6 +1,14 @@
 library(testthat)
-library(dplyr)
-source("x2y.R")
+test_that("x2y works high level of missing values",{
+  x <- rep(NA, 1000)
+  x[34] <- 71
+  y <- rep(NA, 1000)
+  y[107] <- 1
+  x2y_mi <- x2y(x, y)
+  expect_equal(x2y_mi$perc_of_obs, 0)
+  expect_equal(x2y_mi$x2y, 0)
+})
+
 test_that("x2y works with pure POSIXct",{
   x <- rpois(200, 200) %>% as.POSIXct(origin = lubridate::now())
   y <- rpois(200, 200) %>% as.POSIXct(origin = lubridate::now())
@@ -29,7 +37,7 @@ test_that("x2y works with difftimes",{
 
 test_that("x2y works with mixed POSIXct and character",{
   x <- rpois(200, 200) %>% as.POSIXct(origin = lubridate::now())
-  y <- sample(LETTERS, 200, replace = TRUE) 
+  y <- sample(LETTERS, 200, replace = TRUE)
   x2y_mi <- x2y(x, y)
   expect_equal(x2y_mi$perc_of_obs, 100)
   expect_gte(x2y_mi$x2y, 0)
@@ -39,42 +47,43 @@ test_that("x2y works with mixed POSIXct and character",{
 })
 
 test_that("sample_n defaults allows human acceptable compute time",{
-  x <- rpois(1e4, 200) 
-  y <- rpois(1e4, 200) 
-  
+  skip_on_cran()
+  x <- rpois(1e4, 200)
+  y <- rpois(1e4, 200)
+
   tictoc::tic()
   x2y_mi <- x2y(x, y)
   time <- tictoc::toc()
   duration1 <- time$toc - time$tic
-  
-  x <- rpois(1e7, 200) 
-  y <- rpois(1e7, 200) 
-  
+
+  x <- rpois(1e7, 200)
+  y <- rpois(1e7, 200)
+
   tictoc::tic()
   x2y_mi <- x2y(x, y)
   time <- tictoc::toc()
   duration2 <- time$toc - time$tic
-  
+
   expect_lte(abs(duration1 - duration2), 1)
 })
 
 test_that("sample_n works",{
-  x <- rpois(1e4, 200) 
-  y <- rpois(1e4, 200) 
-  
+  x <- rpois(1e4, 200)
+  y <- rpois(1e4, 200)
+
   tictoc::tic()
   x2y_mi <- x2y(x, y, sample_n=NULL)
   time <- tictoc::toc()
   duration1 <- time$toc - time$tic
-  
-  x <- rpois(1e6, 200) 
-  y <- rpois(1e6, 200) 
-  
+
+  x <- rpois(1e6, 200)
+  y <- rpois(1e6, 200)
+
   tictoc::tic()
   x2y_mi <- x2y(x, y, sample_n=NULL)
   time <- tictoc::toc()
   duration2 <- time$toc - time$tic
-  
+
   expect_gte(abs(duration1 - duration2), 1)
 })
 
