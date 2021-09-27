bb0e4e0250ec30573c27810bd34437bc2b7d6a0e
Christophe Regouby <christophe.regouby@free.fr>
2021-09-27 17:36:25 +0200

    bigger dataset with factor lumped for smaller disk footprint

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Grocery products facts.
+#'
+#' A dataset containing the ingredients and other attributes of a sample of 200
+#' grocery store products
+#'
+#' @format A data frame with 1000 rows and 59 variables:
+#' \describe{
+#'   \item{product_name}{product name}
+#'   \item{nutriscore_grade}{a grading of the nutricial quality of the product}
+#'   \item{nova_group}{a grading of the additive present in the product}
+#'   \item{*_100g}{quantity of element per 100g of the product}
+#'   ...
+#' }
+#' @source \url{https://world.openfoodfacts.org/data/data-fields.txt}
+"openfoodfacts"
